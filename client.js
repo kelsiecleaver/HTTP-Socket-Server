b9bd967d3f4b0692c921413804a49c23754bcc4c
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-06 19:06:26 -1000

    server talking to client

--- client.js
+++ client.js
@@ -1,3 +1,13 @@
 /*jshint esversion: 6 */
 
 const net = require('net');
+const client = net.connect(8080, () => {
+  console.log('connected to server');
+  client.on('data', (chunk) =>{
+    console.log(chunk.toString());
+
+  });
+  process.stdin.on('data', (chunk) => {
+    client.write(chunk);
+  });
+});
