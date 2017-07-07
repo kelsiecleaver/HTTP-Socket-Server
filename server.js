73a5b8d129db53505ca5d04a8e4d47ba791462aa
Kelsie Cleaver <kelsiecleaver@Kelsies-MacBook-Pro.local>
2017-07-06 19:06:44 -1000

    server communicating

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 /*jshint esversion: 6 */
 const net = require('net');
+const fs = require('fs');
+
+let users = [];
+const server = net.createServer((request) => {
+  users.push(request);
+
+  request.on('data', (chunk) => {
+    process.stdout.write(chunk);
+    users.forEach((client) => {
+      if(client === request) return;
+    });
+  });
+  process.stdin.on('data', (command) => {
+    request.write(command);
+  });
+});
+server.listen(8080, () => {
+  console.log('opened server on address');
+});
